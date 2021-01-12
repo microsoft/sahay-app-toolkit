dd5109772dfac624f2c1ec3334a0078f47343035
Prateek Chanda <v-pratec@microsoft.com>
2021-01-12 09:47:37 +0530

    add a common sahay framework decider

---
+++ Sahay/App.types.ts
@@ -0,0 +1,7 @@
+import { NavigationScenes } from "./src/AppNavigator/AppNavigator.types";
+
+export interface IFlatListCompData {
+    componentName : string;
+    goto : NavigationScenes
+    // Add gotoProps later
+}
